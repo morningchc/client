7d9aa404ae99771b4485c013fddc38456d6a8762
cao'chen <cao'chen@163.com>
2018-09-07 17:43:01 +0800

    m

---
+++ src/main/java/com/example/demo/controller/Welcome.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/9/7.
+ */
+@RestController
+@RequestMapping("/welcome")
+public class Welcome {
+}
